a61edd1b9baedfbb319601d8a7ea6d57295a848e
Maciej Świszczowski <swiszczak@gmail.com>
2020-06-13 16:40:59 +0200

    Comments added

--- Framework/App.xaml.cs
+++ Framework/App.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Globalization;
+using System.Threading;
 using System.Windows;
 
 namespace Framework
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("En-US");
+        }
     }
 }
